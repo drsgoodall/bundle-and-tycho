f630d0a0ba7b5e6542491ce2bb802a1d6d795ea8
Simon Goodall <simon@simongoodall.co.uk>
2012-07-23 22:22:10 +0100

    Initial commit

---
+++ example-bundle/src/main/java/com/example/ExampleClass.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Dummy class
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class ExampleClass {
+
+	public String doSomething() {
+		return "something";
+	}
+}
